5776ef1a6f41830b17356c9eb0a66e734db07e3c
oarrubla <87502438+oarrubla@users.noreply.github.com>
2023-12-20 16:05:43 -0500

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,15 @@
+fruit = "banana"
+
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print(letter)
+    index += 1
+
+for letter in fruit:
+    print(letter)
+
+s = "Monty python"
+print(s[0:4])
+print(s[6:7])
+print(s[6:20])
---
+++ strings_2.py
@@ -0,0 +1,47 @@
+stuff = "Hello world"
+print(type(stuff))
+print(dir(stuff)) #aparece todo lo que puedes hacer con la clase string
+
+
+#in para buscar dentro de un string
+fruit = "banana"
+print("n" in fruit)
+print("m" in fruit)
+print("nan" in fruit)
+if "a" in fruit:
+    print("Found it")
+
+greet = "Hello Bob"
+zap = greet.lower() #esta funcion/metodo lower crea una copia del string inicial y no lo modifica
+print(zap)
+print(greet)
+print("Hi there".lower())
+print(greet.upper()) #poner todo en mayusculas
+
+
+#find() funcion para buscar algo en todo el string y arroja el index. si no existe arroja -1
+fruit = "banana"
+pos =fruit.find("na")
+print(pos)
+aa = fruit.find("z")
+print(aa)
+
+#search and replace
+greet = "Hello Bob"
+nstr = greet.replace("Bob", "Jane")
+print(nstr)
+
+nstr = greet.replace("o","X")
+print(nstr)
+
+#strtipping whitespaces - remover espacios en blanco
+#lstrip() remueve isquierda, rstrip() remueve derecha, strip()remueve
+greet = "     Hello Bob      "
+print(greet.lstrip())
+print(greet.rstrip())
+print(greet.strip())
+
+#Prefixes 
+line = "Please have a nice day"
+print(line.startswith("Please"))
+print(line.startswith("p"))
